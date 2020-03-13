1c6cfa06465a7f541f4828725a86ffb3ee6e0be5
lilongsheng <lilongsheng@01zhuanche.com>
2020-03-13 12:21:39 +0800

    添加工具类

---
+++ br-common/src/main/java/com/br/utils/DateUtils.java
@@ -0,0 +1,8 @@
+package com.br.utils;
+
+public class DateUtils {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ br-common/src/main/java/com/br/utils/StringUtils.java
@@ -0,0 +1,9 @@
+package com.br.utils;
+
+public class StringUtils {
+
+    public static void main(String[] args) {
+        System.out.println(String.format("%012f",33.45));
+    }
+
+}
