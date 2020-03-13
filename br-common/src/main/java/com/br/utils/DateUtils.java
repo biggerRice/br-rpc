6f27636ca8268ed72f79cb7e9c100e2b9790305b
lilongsheng <lilongsheng@01zhuanche.com>
2020-03-13 16:02:50 +0800

    create new branch feature1

--- br-common/src/main/java/com/br/utils/DateUtils.java
+++ br-common/src/main/java/com/br/utils/DateUtils.java
@@ -4,5 +4,7 @@ public class DateUtils {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("create new branch feature1");
     }
 }
