a6169d46f31dd132d76b6873c610e61df332e356
lilongsheng <lilongsheng@01zhuanche.com>
2020-03-16 14:10:30 +0800

    202003161408 修改stringUtils.java

--- br-common/src/main/java/com/br/utils/StringUtils.java
+++ br-common/src/main/java/com/br/utils/StringUtils.java
@@ -5,6 +5,7 @@ public class StringUtils {
     public static void main(String[] args) {
 
         System.out.println("master branch create a commit");
+        System.out.println("202003161408 修改stringUtils.java字符串工具类");
     }
 
 }
