aa076d31a782d2c03deb38e360a5f396aa2255b8
lilongsheng <lilongsheng@01zhuanche.com>
2020-03-16 16:41:51 +0800

    202003161641 test --amend

--- br-common/src/main/java/com/br/utils/DateUtils.java
+++ br-common/src/main/java/com/br/utils/DateUtils.java
@@ -5,6 +5,8 @@ public class DateUtils {
     public static void main(String[] args) {
     	if(true){
 		System.out.prinln("202003161506 修改日期工具类");
+
+		System.out.prinln("202003161506");
 	}
     }
 
